2ff89b12d2e38b1022d50a46592a4dff4eab5bf3
Ptunc <spolentunc@gmail.com>
2022-10-26 10:41:05 +0300

    just commit

---
+++ java35.java
@@ -0,0 +1,21 @@
+package Java35;
+import java.util.Scanner;
+
+public class java35 {
+
+    public static void main(String[] args) {
+        usAlma();
+    }
+
+    static int usAlma(){
+        Scanner inp = new Scanner(System.in);
+        int a, b, top=1;
+        System.out.print("Sayıyı ve üssü giriniz: ");
+        a = inp.nextInt();
+        b = inp.nextInt();
+        for(int i=1; i<=b; i++) {
+            top *=a;
+        }System.out.println(top);
+        return usAlma();
+    }
+}
